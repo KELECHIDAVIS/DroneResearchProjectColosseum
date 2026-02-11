0f04b4665c3b366c3613b5eb742437a6c09aa489
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2026-02-11 11:15:45 -0800

    Was finally able to get movement working

--- drone_rl_project/test_connection.py
+++ drone_rl_project/test_connection.py
@@ -1,26 +1,35 @@
-# simple_test.py
+import setup_path
 import airsim
-import time
 
-print("Connecting...")
+import numpy as np
+import os
+import tempfile
+import pprint
+import cv2
+
+# connect to the AirSim simulator
 client = airsim.MultirotorClient()
 client.confirmConnection()
-
 client.enableApiControl(True)
-client.armDisarm(True)
 
-print("Taking off...")
-client.takeoffAsync().join()
+state = client.getMultirotorState()
+s = pprint.pformat(state)
+print("state: %s" % s)
 
-print("Flying in a square pattern...")
-client.moveToPositionAsync(5, 0, -5, 2).join()
-client.moveToPositionAsync(5, 5, -5, 2).join()
-client.moveToPositionAsync(0, 5, -5, 2).join()
-client.moveToPositionAsync(0, 0, -5, 2).join()
 
-print("Landing...")
-client.landAsync().join()
+state = client.getMultirotorState()
+print("state: %s" % pprint.pformat(state))
+
+client.moveToPositionAsync(10, 0, -10, 5).join()
+client.moveToPositionAsync(10, 10, -10, 5).join()
+client.moveToPositionAsync(0, 10, -10, 5).join()
+client.moveToPositionAsync(0, 0, -10, 5).join()
+
+
+
+print("Resetting to original state...")
+client.reset()
 client.armDisarm(False)
-client.enableApiControl(False)
 
-print("Done!")
+# that's enough fun for now. let's quit cleanly
+client.enableApiControl(False)
